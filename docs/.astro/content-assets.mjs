9df3cc1aac3e251bdad3086e7e87109922eb64cd
Chris Hasson <hassoncs@gmail.com>
2026-01-15 12:56:15 -0800

    fix: crop hero image to remove watermark
    - Crop 10% from left/right, 5% from top/bottom
    - New dimensions: 2252x1382 (from 2816x1536)
    - Original preserved in hero_original_backup.png

--- docs/.astro/content-assets.mjs
+++ docs/.astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_1H7kN4 from "../../assets/images/hero.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/images/hero.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_1H7kN4]]);
+		
