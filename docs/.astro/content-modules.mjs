64cf3561dd16707970a248679415d84d68cd25fc
Chris Hasson <hassoncs@gmail.com>
2026-01-16 07:47:32 -0800

    build(repo): migrate from pnpm to bun

--- docs/.astro/content-modules.mjs
+++ docs/.astro/content-modules.mjs
@@ -2,12 +2,12 @@
 export default new Map([
 ["src/content/docs/getting-started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/guides/ai-prompts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fai-prompts.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/ai-native.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fai-native.mdx&astroContentModuleFlag=true")],
+["src/content/docs/guides/configuration.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fconfiguration.mdx&astroContentModuleFlag=true")],
+["src/content/docs/guides/monorepos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fmonorepos.mdx&astroContentModuleFlag=true")],
+["src/content/docs/guides/ai-prompts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fai-prompts.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/reference/commands.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fcommands.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/migrations/v2-to-v3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmigrations%2Fv2-to-v3.mdx&astroContentModuleFlag=true")],
-["src/content/docs/guides/monorepos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fmonorepos.mdx&astroContentModuleFlag=true")],
-["src/content/docs/guides/configuration.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fconfiguration.mdx&astroContentModuleFlag=true")],
-["src/content/docs/reference/formats.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fformats.mdx&astroContentModuleFlag=true")],
-["src/content/docs/reference/files.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Ffiles.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/reference/files.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Ffiles.mdx&astroContentModuleFlag=true")],
+["src/content/docs/reference/formats.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fformats.mdx&astroContentModuleFlag=true")]]);
 		
